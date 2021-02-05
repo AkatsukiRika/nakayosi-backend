2de38c7810ba8727a5bde5b0217a866a103cc457
dengrunming <dengrunming@bytedance.com>
2021-02-05 16:12:13 +0800

    0.2.2: 移除CSV数据和数据处理脚本，减小Git仓库体积

--- state.js
+++ state.js
@@ -1,7 +1,7 @@
 const state = {
     // global variables
     PROGRAM_NAME: 'nakayosi-backend',
-    VERSION_CODE: '0.2.1 DEV',
+    VERSION_CODE: '0.2.2 DEV',
     // variables concerning elasticsearch
     ELASTIC_ADDR: 'http://127.0.0.1:9200',
     ELASTIC_MAIN_SUFFIX: '/question/main',
